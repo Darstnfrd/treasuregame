903604928f68891f5fd5c85fa2bf1451a41fda20
Darstnfrd <36829458+Darstnfrd@users.noreply.github.com>
2019-06-11 02:05:51 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php include('header.php'); ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Treasure Game</title>
+  <link rel="stylesheet" href="css/main.css">
+  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  <link rel="icon" href="images/favicon.png">
+</head>
+<body>
+
+<div class="main">
 
 <div class="container">
 	
@@ -21,4 +35,10 @@
 	
 </div>
 
-<?php include('footer.php'); ?>
+</div>
+</body>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="js/game.js"></script>
+
+</html>
